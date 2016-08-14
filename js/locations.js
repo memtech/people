a028285d35fc1d19e2ad02b9efe73eae2bb6ec99
Brad Montgomery <brad@bradmontgomery.net>
2016-08-14 12:39:12 -0500

    updating memphis coords

--- js/locations.js
+++ js/locations.js
@@ -1,5 +1,5 @@
 var locations = [
   { name: 'wayne bills', origin: 'northaven, tn', latitude: 35.26507, longitude: -90.04078 },
   { name: 'brad montgomery', origin: 'lepanto, ar', latitude: 35.61046, longitude: -90.33114 },
-  { name: 'stephen bramlett', origin: 'memphis, tn', latitude: 35.61195, longitude: -90.33113 }
+  { name: 'stephen bramlett', origin: 'memphis, tn', latitude: 35.14174, longitude: -89.97456 }
 ];
