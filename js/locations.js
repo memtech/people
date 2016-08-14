7ea6bd3d4c6b886c7a02063513ce126c24d90568
sbramlett <stephenbramlett@gmail.com>
2016-08-14 18:00:26 -0500

    locations.js
    updated my accurate location. It was still showing midtown for some reason.

--- js/locations.js
+++ js/locations.js
@@ -1,6 +1,6 @@
 var locations = [
   { name: 'wayne bills', origin: 'northaven, tn', latitude: 35.26507, longitude: -90.04078 },
   { name: 'brad montgomery', origin: 'lepanto, ar', latitude: 35.61046, longitude: -90.33114 },
-  { name: 'stephen bramlett', origin: 'memphis, tn', latitude: 35.14174, longitude: -89.97456 },
+  { name: 'stephen bramlett', origin: 'memphis, tn', latitude: 35.2246054, longitude: -90.0001485 },
   { name: 'eric terpstra', origin: 'kellogg, ia', latitude: 41.776711, longitude:-92.964886 }
 ];
