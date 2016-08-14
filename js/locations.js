0c9903b68b71240ee4e660e6f556aa384edb8a42
Brad Montgomery <brad@bradmontgomery.net>
2016-08-14 12:26:23 -0500

    include our info in locations.js

--- js/locations.js
+++ js/locations.js
@@ -1,3 +1,5 @@
 var locations = [
-  { name: 'wayne bills', origin: 'northaven, tn', latitude: 35.26507, longitude: -90.04078 }
+  { name: 'wayne bills', origin: 'northaven, tn', latitude: 35.26507, longitude: -90.04078 },
+  { name: 'brad montgomery', origin: 'lepanto, ar', latitude: 35.61046, longitude: -90.33114 },
+  { name: 'stephen bramlett', origin: 'memphis, tn', latitude: 35.61195, longitude: -90.33113 }
 ];
