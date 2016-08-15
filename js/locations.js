1a8efa78722eb174f75b6472a14e8d6f9ed4f771
Josh W Lewis <josh.w.lewis@gmail.com>
2016-08-14 19:11:20 -0500

    Add Josh Lewis

--- js/locations.js
+++ js/locations.js
@@ -2,5 +2,6 @@ var locations = [
   { name: 'wayne bills', origin: 'northaven, tn', latitude: 35.26507, longitude: -90.04078 },
   { name: 'brad montgomery', origin: 'lepanto, ar', latitude: 35.61046, longitude: -90.33114 },
   { name: 'stephen bramlett', origin: 'memphis, tn', latitude: 35.14174, longitude: -89.97456 },
-  { name: 'eric terpstra', origin: 'kellogg, ia', latitude: 41.776711, longitude:-92.964886 }
+  { name: 'eric terpstra', origin: 'kellogg, ia', latitude: 41.776711, longitude:-92.964886 },
+  { name: 'josh lewis', origin: 'rogersville, mo', latitude: 37.110618, longitude: -93.053759 }
 ];
