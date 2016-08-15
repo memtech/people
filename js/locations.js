a2c0f96121cd92ab0deda2771e26c32c6c84933f
Joe Ferguson <joe@joeferguson.me>
2016-08-15 16:52:46 -0500

    Add Joe & .gitignore

--- js/locations.js
+++ js/locations.js
@@ -4,5 +4,6 @@ var locations = [
   { name: 'stephen bramlett', origin: 'memphis, tn', latitude: 35.2246054, longitude: -90.0001485 },
   { name: 'eric terpstra', origin: 'kellogg, ia', latitude: 41.776711, longitude:-92.964886 },
   { name: 'josh lewis', origin: 'rogersville, mo', latitude: 37.110618, longitude: -93.053759 },
-  { name: 'Daniel Pritchett', origin: 'Dothan, AL', latitude: 31.2290238, longitude: -85.4766258 }
+  { name: 'Daniel Pritchett', origin: 'Dothan, AL', latitude: 31.2290238, longitude: -85.4766258 },
+  { name: 'Joe Ferguson', origin: 'memphis, tn', latitude: 35.2246054, longitude: -90.0001485 },
 ];
