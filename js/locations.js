22fdb502bce39bef145349b339ca981522684a66
David Hiltenbrand <David.E.Hiltenbrand@gmail.com>
2016-08-15 17:14:11 -0500

    Update locations.js

--- js/locations.js
+++ js/locations.js
@@ -4,5 +4,6 @@ var locations = [
   { name: 'stephen bramlett', origin: 'memphis, tn', latitude: 35.2246054, longitude: -90.0001485 },
   { name: 'eric terpstra', origin: 'kellogg, ia', latitude: 41.776711, longitude:-92.964886 },
   { name: 'josh lewis', origin: 'rogersville, mo', latitude: 37.110618, longitude: -93.053759 },
-  { name: 'Daniel Pritchett', origin: 'Dothan, AL', latitude: 31.2290238, longitude: -85.4766258 }
+  { name: 'Daniel Pritchett', origin: 'Dothan, AL', latitude: 31.2290238, longitude: -85.4766258 },
+  { name: 'David Hiltenbrand', origin: 'Pine Bluff, AR', latitude: 34.231984, longitude: -92.135550 }
 ];
