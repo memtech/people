f8613ae741b79776583b6b1d5b95bf19ae117f06
sbramlett <stephenbramlett@gmail.com>
2016-08-22 12:52:03 -0500

    location.js
    Made my coords different than Joe's...

--- js/locations.js
+++ js/locations.js
@@ -1,7 +1,7 @@
 var locations = [
   { name: 'wayne bills', origin: 'northaven, tn', latitude: 35.26507, longitude: -90.04078 },
   { name: 'brad montgomery', origin: 'lepanto, ar', latitude: 35.61046, longitude: -90.33114 },
-  { name: 'stephen bramlett', origin: 'memphis, tn', latitude: 35.2246054, longitude: -90.0001485 },
+  { name: 'stephen bramlett', origin: 'memphis, tn', latitude: 35.2243923520236, longitude: -90.00089585781097 },
   { name: 'eric terpstra', origin: 'kellogg, ia', latitude: 41.776711, longitude:-92.964886 },
   { name: 'josh lewis', origin: 'rogersville, mo', latitude: 37.110618, longitude: -93.053759 },
   { name: 'Daniel Pritchett', origin: 'Dothan, AL', latitude: 31.2290238, longitude: -85.4766258 },
