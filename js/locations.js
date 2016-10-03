8f12057439228e4b19ffae2395b584755611085e
Daniel J. Pritchett <dpritchett@gmail.com>
2016-10-03 10:37:24 -0500

    Adding my slack handle 🎃🎃🎃🎃

--- js/locations.js
+++ js/locations.js
@@ -4,7 +4,7 @@ var locations = [
   { name: 'Stephen Bramlett', origin: 'memphis, tn', latitude: 35.2243923520236, longitude: -90.00089585781097 },
   { name: 'eric terpstra', origin: 'kellogg, ia', latitude: 41.776711, longitude:-92.964886 },
   { name: 'josh lewis', origin: 'rogersville, mo', latitude: 37.110618, longitude: -93.053759 },
-  { name: 'Daniel Pritchett', origin: 'Dothan, AL', latitude: 31.2290238, longitude: -85.4766258 },
+  { name: 'Daniel Pritchett', origin: 'Dothan, AL', latitude: 31.2290238, longitude: -85.4766258, slack_handle: "dpritchett" },
   { name: 'Joe Ferguson', origin: 'memphis, tn', latitude: 35.2246054, longitude: -90.0001485 },
   { name: 'David Hiltenbrand', origin: 'Pine Bluff, AR', latitude: 34.231984, longitude: -92.135550 },
   { name: 'Ismael Alonso', origin: 'Valladolid, ES-VA', latitude: 41.6519094, longitude: -4.730486 },
