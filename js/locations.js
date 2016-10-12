c860f2bf6ea185f9539bfc86982ff3fbf85c4769
Forrest Williams <forrest.williams@gmail.com>
2016-10-12 14:06:16 -0500

    Add company
    Trying a github inline edit for experimental purposes; feel free to decline if it looks weird.

--- js/locations.js
+++ js/locations.js
@@ -14,6 +14,6 @@ var locations = [
   { name: 'Jason Myers', origin: 'Sacramento, CA', latitude: 38.601524,longitude: -121.392555,company: 'Shelby Systems', slack_handle: "syliddar"  },
   { name: 'Daniel Soskel', origin: 'Salt Lake City, UT', latitude: 40.7762692, longitude: -112.2006695, slack_handle: "meltheous" },
   { name: 'Mike Cochran', origin: 'Memphis, TN', latitude: 35.15497509873826, longitude: -90.01109898090363, slack_handle: 'vongrippen' },
-  { name: 'Forrest Williams', origin: 'Memphis, TN', latitude: 35.036800, longitude: -89.823446, slack_handle: 'serroft' },
+  { name: 'Forrest Williams', origin: 'Memphis, TN', latitude: 35.036800, longitude: -89.823446, company: "Mosaik", slack_handle: 'serroft' },
   { name: 'George Spake', origin: 'Memphis, TN', latitude: 35.1182222, longitude: -90.0053115, slack_handle: 'gpspake' }
 ];
