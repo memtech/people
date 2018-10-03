025f1e6ca5d2158759231f9fdbe6e5ad31f38390
Xavier Tilley <hippietilley@gmail.com>
2018-10-03 13:50:50 -0500

    Added myself

--- js/locations.js
+++ js/locations.js
@@ -27,4 +27,5 @@ var locations = [
   { name: 'Cameron Roe', origin: 'Memphis, TN', latitude: 35.1545029, longitude: -89.8414891, company: 'CrossBrowserTesting', slack_handle: 'cameronroe'},
   { name: 'Matthew Bledsoe', origin: 'Memphis, TN', latitude: 35.110297, longitude: -89.914127, company: 'Quire Data', slack_handle: 'mjb'},
   { name: 'Chris House', origin: 'Green Bay, WI', latitude: 44.5228598, longitude: -88.130578, company: 'LDC', slack_handle: 'chris'}
+  { name: 'Xavier Tilley', origin: 'Booneville, MS', latitude: 34.856766,ongitude: -89.6450667, slack_handle: 'chili-tilley'}
   ];
